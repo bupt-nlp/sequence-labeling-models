caf8f15aeb138d2b7ad190ed815dfcdecc6d6e54
wj-Mcat <1435130236@qq.com>
2021-06-04 19:03:06 +0800

    update dataprocess script

--- utils/dataset_processor.py
+++ utils/dataset_processor.py
@@ -1,5 +1,31 @@
 from __future__ import annotations
 
+from typing import List, Union
+
+
+def read_line(line: str) -> Union[List[str], List[str]]: 
+    word, label = list(line.split(' '))
+    tokens = list(word)
+    if label == 'O':
+        return tokens, ['O']* len(tokens)
+
+    labels = []
+
+    labels.append(f'B-{label}')
+    if len(tokens) == 1:
+        return tokens, labels
+    if len(tokens) == 2:
+        labels.append(f'E-{label}')
+        return tokens, labels
+
+    for i in range(1, len(tokens)-1):
+        labels.append(f'I-{label}')
+    labels.append(f'E-{label}')
+    assert len(labels) == len(tokens)
+
+    return tokens, labels 
+    
+
 def convert_bmes_to_sequence_tagging(source_file: str, output_file: str):
     """convert_bmes_to_sequence_tagging convert bbmes format data to sequence-tagging data format
 
@@ -7,5 +33,19 @@ def convert_bmes_to_sequence_tagging(source_file: str, output_file: str):
         source_file (str): the path of bmes format file
         output_file (str): the output file
     """
+    # 1. read all lines and split it to sentences
+    sentences: List[str] = []
+    labels: List[str] = []
+    with open(source_file, 'r+', encoding='utf-8') as f:
+        temp_tokens, temp_labels = [], []
+        for line in f:
+            if not line:
+                sentences.append(temp_tokens)
+                labels.append(temp_labels)
+                temp_tokens, temp_labels = [], []
+            else:
+                
+                
+                
+            
         
-    
