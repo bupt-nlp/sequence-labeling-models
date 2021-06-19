75bb8c24098ad9f307605fad61d811bcd299cfcc
wj-Mcat <1435130236@qq.com>
2021-06-19 19:28:13 +0800

    add cross-bert

--- src/dataset_reader.py
+++ src/dataset_reader.py
@@ -3,3 +3,19 @@ dataset reader
 """
 
 from allennlp.data.dataset_readers import DatasetReader
+from transformers import BertTokenizerFast
+
+
+class JointDatasetReader(DatasetReader):
+    def __init__(self, tokenizer: BertTokenizerFast):
+        self.tokenizer = tokenizer
+
+    def _read_intents(self):
+        pass
+
+    def read_slots(self):
+        pass
+
+    def _read(self):
+        pass
+    
